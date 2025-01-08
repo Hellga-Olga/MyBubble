14137f3a94928f813ecbc690c3cad1c3733a51f8
Hellga-Olga <olga_neyasova@yahoo.com>
2025-01-08 11:17:34 +0000

    update

--- config.py
+++ config.py
@@ -2,11 +2,11 @@ import os
 from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-load_dotenv(os.path.join(basedir, 'my_bubble.env'))
+load_dotenv(os.path.join(basedir, '.env'))
 
 # The configuration class for this application
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
