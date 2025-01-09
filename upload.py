53f8f307aef4d2383e215e06691c7ac320803c15
Hellga-Olga <olga_neyasova@yahoo.com>
2025-01-09 06:37:24 +0000

    css/html update

--- upload.py
+++ upload.py
@@ -2,7 +2,6 @@ import imghdr
 
 
 def validate_image(stream):
-    print(12121212)
     header = stream.read(512)
     stream.seek(0)
     format = imghdr.what(None, header)
@@ -12,7 +11,6 @@ def validate_image(stream):
 
 # defines if files exist
 def file_exist(stream):
-    print('does file exist?')
     header = str(stream.read(512))
     stream.seek(0)
     if header != "b''":
